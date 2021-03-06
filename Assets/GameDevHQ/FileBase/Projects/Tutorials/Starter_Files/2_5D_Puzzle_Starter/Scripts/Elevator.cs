7167823fb133db03167419ce748055b4cc4c8db4
Mo Moxi <mem53211@gmail.com>
2021-03-06 19:51:34 +0000

    Wall Jumping

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/Elevator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Elevator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetA, _targetB;
+    private bool _goingDown = false;
+    [SerializeField]
+    private float _speed = 2.5f;
+
+public void CallElevator()
+{
+    _goingDown = !_goingDown;
+}
+
+void FixedUpdate()
+{
+    if (_goingDown == true)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _targetB.position, _speed * Time.deltaTime);
+    }
+    else if (_goingDown == false)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _targetA.position, _speed * Time.deltaTime);
+    }    
+}
+private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+private void OnTriggerExit(Collider other)
+{
+    if (other.CompareTag("Player"))
+    {
+        other.transform.parent = null;
+    }
+}
+}
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/Elevator_Panel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator_Panel : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _CallButton;
+    [SerializeField] private GameObject _elevator;
+    [SerializeField] private int _requiredCoins;
+    private bool _elevatorCalled;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E) && other.GetComponent<Player>().CoinCount() >= _requiredCoins)
+            {
+                if (_elevatorCalled == true)
+                {
+                    _CallButton.material.color = Color.red;
+                }
+                else
+                {
+                    _CallButton.material.color = Color.green;
+                    _elevatorCalled = true;
+                }
+
+                _elevator.GetComponent<Elevator>().CallElevator();
+            }
+        }
+    }
+}
