8d35414f8583e274938d7bf98da7c1da25249b9c
Mo Moxi <mem53211@gmail.com>
2021-03-06 19:51:34 +0000

    Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
+
+    [SerializeField] private float _jumpHeight = 15.0f;
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+    void FixedUpdate()
+    {
+        float _horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 direction = new Vector3(_horizontalInput, 0, 0);
+        Vector3 velocity = direction * _speed;
+
+        if (_controller.isGrounded == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity.y = _jumpHeight;
+            }
+        }
+        else
+        {
+            velocity.y -= _gravity;
+        }
+
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
